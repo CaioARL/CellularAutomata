57b4119132f035d560f1c79dfc8150ec7b03e25e
Gabriel Alves <102634725+GabrielAlvesGit@users.noreply.github.com>
2023-06-03 16:53:08 -0300

    Add files via upload

---
+++ CellularAutomataNeighborhood.java
@@ -0,0 +1,9 @@
+package br.edu.sp.mackenzie.ppgeec.ca.neighborhood;
+
+import br.edu.sp.mackenzie.ppgeec.ca.state.CellularAutomataState;
+
+public interface CellularAutomataNeighborhood {
+
+	public abstract CellularAutomataState[][] goToNextGeneration(CellularAutomataState[][] currentGeneration);
+
+}
